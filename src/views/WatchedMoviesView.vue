04575529ae25beede4b3096253a907558094527c
Erwan Lajouannique <er.lajouannique@gmail.com>
2024-02-02 13:38:15 +0100

    Watched movies using api

--- src/views/WatchedMoviesView.vue
+++ src/views/WatchedMoviesView.vue
@@ -2,6 +2,7 @@
 import axios from 'axios'
 import MovieCard from '../components/MovieCard.vue'
 import type { IMovie } from '@/shared/types'
+import { fetchMoviesByIds } from '../services/tmdbApi'
 
 export default {
   name: 'WatchedMovies',
@@ -19,35 +20,26 @@ export default {
   },
   methods: {
     async loadWatched() {
-      this.isLoading = true;
-      const movieIds = [];
+      this.isLoading = true
+      const movieIds: string[] = []
 
       for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i) || '';
-        const value = localStorage.getItem(key);
-        if (key && key.startsWith('watched-')) {
-          const movieId = key.split('-')[1];
-          if (value === "true") {
-            movieIds.push(movieId);
+        const key = localStorage.key(i) || ''
+        if (key.startsWith('watched-')) {
+          const movieId = key.split('-')[1]
+          const value = localStorage.getItem(key)
+          if (value === 'true') {
+            movieIds.push(movieId)
           }
         }
       }
 
-      const moviePromises = movieIds.map((id) =>
-        axios.get(`https://api.themoviedb.org/3/movie/${id}?language=en-US`, {
-          headers: {
-            Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`
-          }
-        })
-      )
-
       try {
-        const movies = await Promise.all(moviePromises);
-        this.watched = movies.map((response) => response.data);
+        this.watched = await fetchMoviesByIds(movieIds)
       } catch (error) {
-        console.error('Error', error);
+        console.error('Error', error)
       } finally {
-        this.isLoading = false;
+        this.isLoading = false
       }
     }
   }
@@ -58,6 +50,7 @@ export default {
   <main>
     <h1>Watched movies</h1>
     <div v-if="isLoading"><span>Loading...</span></div>
+    <span v-else-if="!isLoading && watched.length === 0">No movie in watched list</span>
     <div v-else class="watched-container">
       <MovieCard
         v-for="movie in watched"
@@ -72,7 +65,6 @@ export default {
 </template>
 
 <style scoped>
-
 main {
   padding: 48px 48px 0px 48px;
 }
@@ -87,7 +79,7 @@ h1 {
   flex-wrap: wrap;
 }
 
-@media (max-width : 768px) {
+@media (max-width: 768px) {
   .watched-container {
     justify-content: center;
   }
